cea9be05d9856bdf14b5cacf7f4a7a5b1fa2b432
etricky <etricky+git@gmail.com>
2018-05-14 14:11:10 +0100

    Initial setup

---
+++ src/main/java/com/etricky/cryptobot/CryptoBotApplication.java
@@ -0,0 +1,12 @@
+package com.etricky.cryptobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoBotApplication.class, args);
+	}
+}
